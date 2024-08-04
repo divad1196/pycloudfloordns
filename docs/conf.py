b135a8dc4c8f78d1b5f0c033f58e7bdc45f11de1
Gallay David <david.gallay.96@gmail.com>
2024-08-04 19:15:44 +0200

    Adding readthedocs configuration

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+import sys
+import os
+
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#ensuring-the-code-can-be-imported
+sys.path.insert(0, os.path.abspath(".."))  # noqa: PTH100
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = "CloudfloorDNS"
+copyright = "2024, Gallay David"
+author = "David Gallay"
+
+release = "0.1"
+version = "0.1.0"
+
+# -- General configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+# -- Options for HTML output
+
+html_theme = "sphinx_rtd_theme"
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
+
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+CloudfloorDNS
+===================================
+
+
+.. note::
+
+   The documentation is under development, but the library is stable. You can look at the github's repository for now.
+
+
+.. automodule:: cloudfloordns
+   :members:
+   .. :undoc-members:
+   .. :show-inheritance:
